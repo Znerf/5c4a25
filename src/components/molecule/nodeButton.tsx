ec883f55bf8ac1fdc0c01619380cbc5cc1c2afd3
Sagar <znerfii.a1@gmail.com>
2025-05-21 11:45:41 -0700

    feat(nodeButton): handled all the onClick funcation and resturn to state

--- src/components/molecule/nodeButton.tsx
+++ src/components/molecule/nodeButton.tsx
@@ -2,14 +2,16 @@ import { Node } from "@/types/api/blueprint"
 
 interface NodeButtonProps {
     node: Node
+    onClick: (node: Node) => void
 }
 
-export const NodeButton = ({node}: NodeButtonProps) =>{
+export const NodeButton = ({node, onClick}: NodeButtonProps) =>{
     return( 
         <button
             id = {node.id}
             className="absolute bg-blue-500 text-white font-bold py-2 px-4 rounded"
             style={{ left: `${node.position.x-300}px`, top: `${node.position.y}px` }}
+            onClick={() => onClick(node)}
         >
             {node.data.name}
         </button>
--- src/components/organism/diagram.tsx
+++ src/components/organism/diagram.tsx
@@ -1,11 +1,17 @@
 'use client';
 import { useGetBlueprintsQuery } from "@/store/service/fetchApi";
-import { Node } from "@/types/api/blueprint";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
 import { NodeButton } from "../molecule/nodeButton";
+
+import { Node } from "@/types/api/blueprint";
+
 
 export default function Diagram() {
     const { data, error, isLoading } = useGetBlueprintsQuery("blueprintId");
+
+    const [selectedNode, setSelectedNode] = useState<Node | null>(null);
+
 
     useEffect(() => {
         if (data) {
@@ -15,12 +21,20 @@ export default function Diagram() {
             console.error("Error:", error);
         }
     })
+    
+    const handleNodeClick = (node: Node) => {
+        setSelectedNode(node);
+    };
+
+    if (isLoading) return <div>Loading...</div>;
+    if (error) return <div>Error loading blueprint</div>;
+    if (!data) return null;
 
     return (
         <div >
             <div> 
                 {data?.nodes.map((node:Node)=>(
-                    <NodeButton key= {node.id} node={node} />
+                    <NodeButton key= {node.id} node={node} onClick={handleNodeClick}/>
                 ))}
             </div>
         </div>
