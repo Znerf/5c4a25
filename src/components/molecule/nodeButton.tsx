ac3aa8efb84be04f5e61bbe049fc382fda08ef45
Sagar <znerfii.a1@gmail.com>
2025-05-21 11:44:00 -0700

    chore: added data interface and corrected name of form

--- src/components/molecule/nodeButton.tsx
+++ src/components/molecule/nodeButton.tsx
@@ -11,8 +11,7 @@ export const NodeButton = ({node}: NodeButtonProps) =>{
             className="absolute bg-blue-500 text-white font-bold py-2 px-4 rounded"
             style={{ left: `${node.position.x-300}px`, top: `${node.position.y}px` }}
         >
-            {node.type}
-           
+            {node.data.name}
         </button>
     )
 }
--- src/types/api/blueprint.tsx
+++ src/types/api/blueprint.tsx
@@ -22,6 +22,17 @@ export interface Node {
 }
 
 export interface NodeData {
+    id: string;
+    component_key: string;
+    component_type: string;
+    component_id: string;
+    name: string;
+    prerequisites: string[];
+    permitted_roles: string[];
+    input_mapping: string[];
+    sla_duration: number;
+    approval_required: boolean;
+    approval_roles: string[];
 }
 export interface Edge {
     id: string;
